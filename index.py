6f5106ef75df26026ee70436f3e69cec12460875
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-16 13:09:40 -0500

    Added CanOpener instance and called open_can method

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from appliances import Dryer
 from appliances import Washer
 from appliances import Refrigerator
 from appliances import CoffeeMaker
+from appliances import CanOpener
 
 whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
@@ -15,3 +16,6 @@ lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+openCan = CanOpener("silver")
+openCan.open_can()
